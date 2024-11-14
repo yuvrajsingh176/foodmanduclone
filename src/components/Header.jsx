22103cef7d1feeba582f6fa5b62273d4f5acfbd2
Yuvraj <ducatimonster729@gmail.com>
2024-11-14 21:50:54 +0545

    added nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import { FaShoppingCart } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className="bg-white p-4 flex  justify-between shadow-lg">
       <img
-        className="h-16 w-20 rounded-full ml-10"
+        onClick={() => {
+          navigate("/");
+        }}
+        className="h-16 w-20 rounded-full ml-10 cursor-pointer"
         src="https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=600"
       />
+
       <div className="flex items-center mr-10 gap-4">
         <FaShoppingCart className="h-10 w-10" />
         <h1>10</h1>
